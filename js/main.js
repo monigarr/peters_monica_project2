e0abbb7e6c98bb4cbdcbbf4abd8d4a767f1aff65
Monica <monigarr@yahoo.com>
2012-02-08 11:52:01 -0500

    add dropdown that is populated via js

--- js/main.js
+++ js/main.js
@@ -10,97 +10,52 @@
 // most  onblur, onclick, ondblclick
 // onfocus, onmousedown, onmouseup, onmouseover, onmouseout
 
-function styleField() {
-	var field = document.getElementById("mname");
-	field.style.backgroundColor = "#c0c0c0";
-}
+// Wait until DOM is ready
+window.addEventListener("DOMContentLoaded", function()
+{
 
-function unstyleField() {
-	var field = document.getElementById("mname");
-	field.style.backgroundColor = "";
-}
-
-function storeMedia(id) {
-	var mtype = document.getElementById("mtype").value;
-	var mname = document.getElementById("mname").value;
-	var mdate = document.getElementById("mdate").value;
-	var mrating = document.getElementById("mrating").value;
-	var mtopic = document.getElementById("mtopic").value;
-	var mtags = document.getElementById("mtags").value;
-	var mcomments = document.getElementById("mcomments").value;
-	localStorage.setMedia("appMediaType", mtype);
-	localStorage.setMedia("appMediaName", mname);
-	localStorage.setMedia("appMediaDate", mdate);
-	localStorage.setMedia("appMediaRating", mrating);
-	localStorage.setMedia("appMediaTopic", mtopic);
-	localStorage.setMedia("appMediaTags", mtags);
-	localStorage.setMedia("appMediaComments", mcomments);
-}
-
-function getAllMedia() {
-	if (localStorage.getMedia("appMediaName")) {
-		var mediaType = localStorage.getMedia("appMediaType");
-		var mediaName = localStorage.getMedia("appMediaName");
-		var mediaDate = localStorage.getMedia("appMediaDate");
-		var mediaRating = localStorage.getMedia("appMediaRating");
-		var mediaTopic = localStorage.getMedia("appMediaTopic");
-		var mediaTags = localStorage.getMedia("appMediaTags");
-		var mediaComments = localStorage.getMedia("appMediaComments");
-		
-}
-
-// clear local storage
-function clearLocal() {
-	clear;
-	localStorage.clear();
-	return false;
-}
-
-/*// wimba sample
-// define vars first
-// then write function
-//getElementById Function
-// function is called $ to select elements
-function $(x) {
-	var theElement = document.getElementById(x);
-	return theElement;
-}
-
-/*
-// vars to target html elements
-// functions with conditional statements to do the work.
-// event listeners to run the functions
-
-// Define Vars
-var pw = $('password');
-var cpw = $('confirm');
-var check = $('submit');
-
-// check to see if password is filled out, if so then make confirm enabled.
-function checkpw() {
-	console.log("pw checked");
-	// use .value to look at what the user typed in the field
-	if(pw.value != "") {
-		cpw.removeAttribute("disabled", "disabled");
-	}else {
-		cpw.setAttribute("disabled", "disabled");
+	// getElementById Function
+	function $(x)
+	{
+		var theElement = document.getElementById(x);
+		return theElement;
 	}
-}
-
-/* 
-//compare passwords
-// on submit, check to see if password field match
-// event data to stop certain default events from happening
-//function comparePasswords(e){
-function comparePasswords() {
-	if(pw.value === cpw.value){
-		alert("The passwords match!");
-	}else{
-		alert("The passwords do not match. Try again");
+	
+	// Create select field element
+	function makeMediaTypes() 
+	{
+		//formTag is an array of all form tags
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $("select"),
+			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id", "mtype");
+		//populate with options
+		for(var i=0, j=mediaGroups.length; i<j; i++) 
+		{
+			//create option for each string in array
+			var makeOption = document.createElement("option");
+			var optText = mediaGroups[i];
+			makeOption.setAttribute("value", optText);
+			//put text somewhere
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
 	}
-}
+	
+	// Variable defaults
+	// store values of dropdown in array
+	var mediaGroups = ["-- Choose Media Type--", "book", "document", "music", "movie", "pdf", "doc", "audio", "video"];
+	makeMediaTypes();
+	
+	// Set Link & Submit Click Events
+	
+	var getAllMedia = $("getAllMedia");
+	getAllMedia.addEventListener("click", getData);
+	var clearStoredMedia = $("clear");
+	clearStoredMedia.addEventListener("click", clearLocal);
+	var storeMedia = $("storeMedia");
+	storeMedia.addEventListener("click", storeData);
+	
+});
 
-//apply event listeners to elements to run the functions that do the work
-pw.addEventListener("blur", checkpw);
-check.addEventListener("click", comparePasswords);
-*/
