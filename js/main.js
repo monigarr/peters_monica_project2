9168b5a402742c02c5fdab4920fdb6ea64f2d796
Monica <monigarr@yahoo.com>
2012-02-08 23:08:07 -0500

    edited flowchart

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-//
 //    Full Sail University
 //    Visual Frameworks
 //    Monica Peters
-//    Web App Part 1
-//    Week 1 Project 1
-//    Due Thursday Feb. 2nd 2012
+//    Web App Part 2
+//    Week 2 Project 2
+//    Due Thursday Feb. 8th 2012
 //
-
-// most  onblur, onclick, ondblclick
-// onfocus, onmousedown, onmouseup, onmouseover, onmouseout
 
 // Wait until DOM is ready
 window.addEventListener("DOMContentLoaded", function()
@@ -57,6 +53,27 @@ window.addEventListener("DOMContentLoaded", function()
 		}
 	}
 	
+	function toggleControls(n)
+	{
+		switch(n)
+		{
+			case "on":
+				$("mediaForm").style.display = "none";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "none";
+				$("addNew").style.display = "inline";
+				break;
+			case "off":
+				$("mediaform").style.display = "block";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "inline";
+				$("addNew").style.display = "none";
+				$("items").style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
 	
 	function saveMedia()
 	{
@@ -85,6 +102,56 @@ window.addEventListener("DOMContentLoaded", function()
 		alert("Media Saved");
 	}
 	
+	function getData()
+	{
+		toggleControls("on");
+		if(localStorage.length === 0)
+		{
+			alert("No Data in local Storage");
+		}
+		//Write Data from Local Storage to the Browswer.
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id","items");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$("items").style.display = "display";
+		
+		for(var i=0, len=localStorage.length; i<len; i++)
+		{
+			var makeli = document.createElement("li");
+			makeList.appendChild(makeli);
+			var key = localStorage.key[i];
+			var value = localStorage.getItem(key);
+			//convert string back to object so it won't be one long string
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement("ul");
+			makeli.appendChild(makeSubList);
+			for(var n in obj)
+			{
+				var makeSubli = document.createElement("li");
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+			}
+		}
+	}
+	
+	function clearLocal()
+	{
+		if(localStorage.length === 0)
+		{
+			alert("No Data to Clear");
+		}
+		else
+		{
+			localStorage.clear();
+			alert("All Media Deleted");
+			window.location.reload();
+			return false;
+		}
+	}
+	
 	// Variable defaults
 	// store values of dropdown in array
 	var mediaGroups = ["-- Choose Media Type--", "book", "document", "music", "movie", "pdf", "doc", "audio", "video"],
@@ -92,11 +159,10 @@ window.addEventListener("DOMContentLoaded", function()
 	makeMediaTypes();
 	
 	// Set Link & Submit Click Events
-	
-	//var getAllMedia = $("getAllMedia");
-	//getAllMedia.addEventListener("click", getData);
-	//var clearStoredMedia = $("clear");
-	//clearStoredMedia.addEventListener("click", clearLocal);
+	var displayLink = $("displayLink");
+	displayLink.addEventListener("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventListener("click", clearLocal);
 	var save = $("submit");
 	save.addEventListener("click", saveMedia);
 	
